483778cb8dbeb95f96d072c4224df879d4515ce8
Jacob Reppuhn <jreppuhn@three-m.com>
2023-10-20 09:23:13 -0400

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000",
+        // secondary: "#000000",
+        // light: "#d9dcd6",
+        // dark: "#16425b",
+      },
+    },
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+    },
   },
   plugins: [],
 };
