ecd722f9f117e87e5c26e51ec8ae48c06f616a3e
Jacob Reppuhn <jreppuhn@three-m.com>
2023-10-23 14:54:52 -0400

    fix some console errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useMatch } from "react-router-dom";
 import Button from "./Button";
-import CopyToClipboard from "../utils/CopyToClipboard";
 
 const Navbar = () => {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
