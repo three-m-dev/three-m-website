4c60b24705897e1dd5df61f390957baef963e040
Jacob Reppuhn <jreppuhn@three-m.com>
2023-11-06 09:09:18 -0500

    loading logic and dynamic content

--- src/components/BulletinContent.tsx
+++ src/components/BulletinContent.tsx
@@ -1,18 +1,106 @@
 // import { useState } from "react";
 
-type Props = {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+
+type ContentSection = {
+  type: "heading" | "subheading" | "paragraph" | "quote" | "image" | "list";
+  content: string | string[];
+  imageUrl?: string;
+  imageAlt?: string;
+};
+
+type Post = {
+  id: string;
   title: string;
+  description: string;
   author: string;
   date: string;
   imageUrl: string;
-  content: string;
+  imageAlt: string;
+  content: ContentSection[];
   type: string;
 };
 
-const BulletinContent = (props: Props) => {
-  // const [loading, setLoading] = useState(true);
+const BulletinContent = () => {
+  const { postId } = useParams();
+  const [post, setPost] = useState<Post | null>(null);
 
-  // const parseContent = () => {};
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("../data/posts.json");
+      const data = await response.json();
+      const foundPost = data.find((p: Post) => p.id === postId);
+      setTimeout(() => {
+        if (foundPost) {
+          setPost(foundPost);
+        }
+      }, 500);
+    };
+
+    fetchPost();
+  }, [postId]);
+
+  if (!post) {
+    return (
+      <div className="flex min-h-[90vh] justify-center">
+        <Loading />
+      </div>
+    );
+  }
+
+  const renderContentSection = (section: ContentSection, index: number) => {
+    switch (section.type) {
+      case "heading":
+        return (
+          <h2 key={index} className="text-2xl font-bold">
+            {section.content}
+          </h2>
+        );
+      case "subheading":
+        return (
+          <h3 key={index} className="text-xl font-semibold">
+            {section.content}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="text-base text-gray-500 md:text-lg">
+            {section.content}
+          </p>
+        );
+      case "quote":
+        return (
+          <blockquote
+            key={index}
+            className="quote border-l-4 border-neutral-500 bg-neutral-100 p-4 italic text-neutral-600"
+          >
+            <p className="mb-2">{section.content}</p>
+          </blockquote>
+        );
+      case "image":
+        return (
+          <div key={index} className="my-4">
+            <img
+              src={section.imageUrl}
+              alt={section.imageAlt}
+              className="w-full"
+            />
+          </div>
+        );
+      case "list":
+        return (
+          <ul key={index} className="ml-5 list-disc">
+            {(section.content as string[]).map((item, itemIndex) => (
+              <li key={itemIndex}>{item}</li>
+            ))}
+          </ul>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <section className="bg-white py-8 md:py-16">
@@ -29,96 +117,20 @@ const BulletinContent = (props: Props) => {
           <div className="w-full px-4 md:w-1/2">
             <p className="mb-2 flex items-center gap-2 text-gray-500">
               <span className="rounded-md bg-gray-200 px-2 py-1 text-xs uppercase tracking-wider text-gray-500">
-                {props.type}
+                {post.type}
               </span>
               <span className="text-sm uppercase tracking-wider">
-                {props.date}
+                {post.date}
               </span>
             </p>
             <h2 className="mb-4 text-3xl font-bold leading-tight tracking-tighter text-gray-900 md:text-4xl lg:text-5xl">
-              {props.title}
+              {post.title}
             </h2>
           </div>
         </div>
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 md:flex-1">
-            <p className="mb-8 border-b border-gray-100 pb-10 text-lg font-medium text-gray-500 md:text-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo
-              massa. Eu dolor aliquet risus gravida nunc at feugiat consequat
-              purus. Non massa enim vitae duis mattis. Vel in ultricies vel
-              fringilla.
-            </p>
-            <h3 className="mb-4 text-2xl font-semibold text-gray-800 md:text-3xl">
-              Header 1
-            </h3>
-            <p className="mb-4 text-base text-gray-500 md:text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo
-              massa. Eu dolor aliquet risus gravida nunc at feugiat consequat
-              purus. Non massa enim vitae duis mattis. Vel in ultricies vel
-              fringilla.
-            </p>
-            <div className="mb-6 border-l-2 border-primary p-6">
-              <p className="mb-4 text-xl font-medium leading-tight text-gray-800 md:text-2xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo
-                massa. Eu dolor aliquet risus gravida nunc at feugiat consequat
-                purus.
-              </p>
-              <span className="text-base font-medium text-gray-400 md:text-lg">
-                — John Doe, CEO &amp; Founder
-              </span>
-            </div>
-            <p className="mb-6 text-base text-gray-500 md:text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo
-              massa. Eu dolor aliquet risus gravida nunc at feugiat consequat
-              purus. Non massa enim vitae duis mattis. Vel in ultricies vel
-              fringilla.
-            </p>
-            <div className="mb-4 max-w-max overflow-hidden rounded-md">
-              <img
-                src="flex-ui-assets/images/blog-content/content-photo2.jpg"
-                alt=""
-              />
-            </div>
-            <p className="mb-8 text-base font-medium text-gray-400 md:text-lg">
-              <span>Non massa enim vitae duis mattis. Vel in</span>
-              <a className="underline hover:text-gray-600" href="#">
-                ultricies
-              </a>
-              <span>vel fringilla.</span>
-            </p>
-            <h3 className="mb-4 text-2xl font-semibold text-gray-800 md:text-3xl">
-              Header 1
-            </h3>
-            <p className="mb-14 text-base text-gray-500 md:text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              ullamcorper mattis lorem non. Ultrices praesent amet ipsum justo
-              massa. Eu dolor aliquet risus gravida nunc at feugiat consequat
-              purus. Non massa enim vitae duis mattis. Vel in ultricies vel
-              fringilla.
-            </p>
-            <p id="summary" className="mb-4 text-base text-gray-500 md:text-lg">
-              Sagittis et eu at elementum, quis in. Proin praesent volutpat
-              egestas sociis sit lorem nunc nunc sit. Eget diam curabitur mi ac.
-              Auctor rutrum lacus malesuada massa ornare et. Vulputate
-              consectetur ac ultrices at diam dui eget fringilla tincidunt. Arcu
-              sit dignissim massa erat cursus vulputate gravida id. Sed quis
-              auctor vulputate hac elementum gravida cursus dis.
-            </p>
-            <ol className="list-inside list-decimal text-base text-gray-500 md:px-5 md:text-lg">
-              <li>Lectus id duis vitae porttitor enim gravida morbi.</li>
-              <li>
-                Eu turpis posuere semper feugiat volutpat elit, ultrices
-                suspendisse. Auctor vel in vitae placerat.
-              </li>
-              <li>
-                Suspendisse maecenas ac donec scelerisque diam sed est duis
-                purus.
-              </li>
-            </ol>
+            {post.content.map(renderContentSection)}
           </div>
         </div>
       </div>
