ce94e06b5dba2fbbd15e08c299c0738f049ec6cf
Jacob Reppuhn <jreppuhn@three-m.com>
2023-11-06 07:58:53 -0500

    bulletin list laoding animation

--- src/components/BulletinList.tsx
+++ src/components/BulletinList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Loading from "./Loading";
 
 type PostType = {
   id: number;
@@ -11,7 +12,9 @@ type PostType = {
 
 const BulletinList = () => {
   const [filter, setFilter] = useState<"all" | "article" | "update">("all");
+
   const [filteredPosts, setFilteredPosts] = useState<PostType[]>([]);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -19,24 +22,67 @@ const BulletinList = () => {
     fetch("../data/posts.json")
       .then((response) => response.json())
       .then((data: PostType[]) => {
-        if (filter === "all") {
-          setFilteredPosts(data);
-        } else {
-          setFilteredPosts(data.filter((post) => post.type === filter));
+        let filteredData = data;
+
+        if (filter !== "all") {
+          filteredData = filteredData.filter(
+            (post) => post.type.toLowerCase() === filter,
+          );
         }
+
+        setTimeout(() => {
+          setFilteredPosts(filteredData);
+          setLoading(false);
+        }, 500);
       })
-      .catch((error) => console.error("Error fetching the posts:", error))
-      .finally(() => setLoading(false));
+      .catch((error) => {
+        console.error("Error fetching the careers:", error);
+        setLoading(false);
+      });
   }, [filter]);
+
+  const renderPosts = () => {
+    if (loading) {
+      return <Loading />;
+    } else if (!loading && filteredPosts.length === 0) {
+      return <div className="w-full">No posts found</div>;
+    } else if (!loading && filteredPosts.length > 0) {
+      return (
+        <div className="flex flex-wrap">
+          {filteredPosts.map((post, index) => (
+            <div key={index} className="mb-8 w-full px-4 sm:mb-0 md:w-1/2">
+              <p className="mb-2 flex items-center gap-2 text-gray-500">
+                <span className="text-sm uppercase tracking-wider">
+                  {post.date}
+                </span>
+                <span className="bg-blue-200 px-2 py-1 text-xs uppercase tracking-wider text-primary">
+                  {post.type}
+                </span>
+              </p>
+              <Link
+                className="mb-2 inline-block text-2xl font-bold leading-tight text-gray-800 hover:underline"
+                to={`/bulletin/` + post.id}
+              >
+                {post.title}
+              </Link>
+              <p className="text-base font-medium text-gray-500 md:text-lg">
+                {post.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  };
 
   return (
     <section className="min-h-[65vh] bg-white py-8 md:py-16">
-      <div className="mx-auto max-w-screen-lg px-4">
+      <div className="mx-auto max-w-screen-xl px-4">
         <div className="mx-auto mb-8 text-center md:mb-8 md:max-w-5xl">
           <span className="mb-4 inline-block rounded bg-blue-200 px-2 py-1 text-sm uppercase leading-5 tracking-wider text-primary">
             News & Insights
           </span>
-          <h1 className="font-bebas mb-4 text-3xl font-bold leading-tight tracking-wider text-gray-800 md:text-5xl">
+          <h1 className="mb-4 font-bebas text-3xl font-bold leading-tight tracking-wider text-gray-800 md:text-5xl">
             Latest News
           </h1>
           <p className="mb-10 text-lg font-medium text-gray-500 md:text-xl">
@@ -77,41 +123,7 @@ const BulletinList = () => {
             </button>
           </li>
         </ul>
-        <div className="-mx-4 flex flex-wrap">
-          {loading ? (
-            <div className="w-full py-10 text-center">
-              <div className="loader"></div>{" "}
-              {/* Replace with your loader animation */}
-              <p>Loading...</p>
-            </div>
-          ) : (
-            filteredPosts.map((post, index) => (
-              <div key={index} className="mb-8 w-full px-4 md:w-1/2">
-                <p className="mb-2 flex items-center gap-2 text-gray-500">
-                  <span className="text-sm uppercase tracking-wider">
-                    {post.date}
-                  </span>
-                  <span className="bg-blue-200 px-2 py-1 text-xs uppercase tracking-wider text-primary">
-                    {post.type}
-                  </span>
-                </p>
-                <Link
-                  className="mb-2 inline-block text-2xl font-bold leading-tight text-gray-800 hover:underline"
-                  to={`/bulletin/` + post.id}
-                >
-                  {post.title}
-                </Link>
-                <p className="text-base font-medium text-gray-500 md:text-lg">
-                  {post.description}
-                </p>
-                <Link
-                  className="inline-flex items-center text-base font-semibold text-primary hover:text-gray-400 md:text-lg"
-                  to={`/bulletin/` + post.id}
-                ></Link>
-              </div>
-            ))
-          )}
-        </div>
+        <div>{renderPosts()}</div>
       </div>
     </section>
   );
